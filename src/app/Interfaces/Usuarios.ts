cc0c686553f1027c80ad6fc05416ae7553adfeff
JAlexanderMatailo <52136249+JAlexanderMatailo@users.noreply.github.com>
2023-09-13 10:16:36 -0500

    creacion componente service e interface usuarios

---
+++ src/app/Interfaces/Usuarios.ts
@@ -0,0 +1,28 @@
+export interface Users{
+    IdUsers? : number;
+    usuario : string;
+    primerNombre : string;
+    segundoNombre : string;
+    primerApellido : string;
+    segundoApellido : string;
+    email : string;
+    estado : boolean;
+    IdDepartamentos : number;
+    IdCargos : number;
+}
+
+export interface Departamentos{
+    IdDepartamento : number;
+    codigoDepartamento : string;
+    nombreDepartamento : string;
+    activo : boolean;
+    idUsuarioCreacion : number;
+}
+
+export interface Cargos{
+    IdCargos : number;
+    codigoCargo : string;
+    nombreCargo : string;
+    activo : string;
+    idUsuarioCreacion : number;
+}
