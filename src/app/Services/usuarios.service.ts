cd8dd5cbee2821be19293d44c18b29ef81e9fb4a
JAlexanderMatailo <52136249+JAlexanderMatailo@users.noreply.github.com>
2023-09-13 10:50:47 -0500

    metodo consultarCargo, correccion de ruta urlbase

--- src/app/Services/usuarios.service.ts
+++ src/app/Services/usuarios.service.ts
@@ -1,9 +1,26 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../environment/environment.develpment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UsuariosService {
+  urlBase = environment.urlBase
+  contoladorUsers = 'Usuarios/'
+   constructor(private http: HttpClient) { }
+ 
+   /*Departamentos*/
+   getDepartamentos(){
+     let header= new HttpHeaders()
+     .set('Type-content','aplication/json')
+     return this.http.get<any>(this.urlBase+this.contoladorUsers+'GetAllDepartamentos');
+   }
 
-  constructor() { }
+  /*Cargos */
+  getCargos(){
+    let header = new HttpHeaders()
+    .set('Type-content','aplication/json')
+    return this.http.get<any>(this.urlBase+this.contoladorUsers+'GetAllCargo');
+  }
 }
--- src/app/home/home/home.component.ts
+++ src/app/home/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { UsuariosService } from 'src/app/Services/usuarios.service';
 
 @Component({
   selector: 'app-home',
@@ -6,5 +8,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.sass']
 })
 export class HomeComponent {
+  public Departamentos : any[]=[];
+  public Cargos : any[]=[];
 
+  constructor(
+    private usuarioService: UsuariosService,
+    private router: Router
+  ){ }
+  ngOnInit(): void {
+    this.obtenerDepartamentos();
+    this.obtenerCargo();
+  }
+  obtenerDepartamentos(){
+    this.Departamentos=[]
+    this.usuarioService.getDepartamentos().subscribe(resp =>{
+      console.log(resp)
+      console.log("hola")
+      this.Departamentos=resp
+    })
+  }
+  obtenerCargo(){
+    this.Cargos=[]
+    this.usuarioService.getCargos().subscribe(resp =>{
+      console.log(resp)
+      this.Cargos=resp
+    })
+  }
 }
